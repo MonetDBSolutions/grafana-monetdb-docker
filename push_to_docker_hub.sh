44741e2e1fd854c3c7739aa0457670e43921ec2b
sstalin <svetlin.stalinov@monetdbsolutions.com>
2017-05-08 14:27:12 +0200

    minor fix

--- push_to_docker_hub.sh
+++ push_to_docker_hub.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-_grafana_tag=$1
-_grafana_version=${_grafana_tag:1:5}
+# _grafana_tag=$1
+# _grafana_version=${_grafana_tag:1:5}
+_grafana_version=$1
 
 if [ "$_grafana_version" != "" ]; then
-	echo "pushing grafana/grafana:${_grafana_version}"
-	docker push grafana/grafana:${_grafana_version}
-	docker push grafana/grafana:latest
+	echo "pushing monetdb/grafana:${_grafana_version}"
+	docker push monetdb/grafana:${_grafana_version}
+	docker push monetdb/grafana:latest
 else
-	echo "pushing grafana/grafana:master"
-	docker push grafana/grafana:master
+	echo "pushing monetdb/grafana:master"
+	docker push monetdb/grafana:master
+	docker push monetdb/grafana:latest
 fi
